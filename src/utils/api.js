84721a3377c7a38ed585ea325b0e44cfe1cfc738
atharvapDexTech <atharvap@dexterrtech.com>
2025-07-05 19:27:39 +0530

    updated the api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000/api';
+const BASE_URL = 'http://server.dexterrtech.com:8000/api/';
 
 
 const api = axios.create({
   baseURL: BASE_URL,
-  timeout: 10000,
+  timeout: 100000,
   headers: {
     'Content-Type': 'application/json',
   },
